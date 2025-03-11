a58607c072c5612e8e39c859bbc50cc91fab83b3
Sneha Ravikumar <160413325+Sneharavikumar06@users.noreply.github.com>
2025-03-11 16:44:13 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/components/*.{js,jsx,ts,tsx}", // Scan all React components
+    "./public/index.html",        // Scan the root HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
