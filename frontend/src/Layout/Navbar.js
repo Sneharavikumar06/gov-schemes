ed99b4e20659630a17c38f69e3476a42861d48b2
Sneha Ravikumar <160413325+Sneharavikumar06@users.noreply.github.com>
2025-03-11 16:52:52 +0530

    Add files via upload

--- frontend/src/Layout/Navbar.js
+++ frontend/src/Layout/Navbar.js
@@ -1,1 +1,19 @@
+import React from "react"; // Add this line
+import { Link } from "react-router-dom";
 
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4 text-white">
+      <div className="container mx-auto flex justify-between">
+        <h1 className="text-xl font-bold">Gov-Schemes</h1>
+        <div>
+          <Link to="/" className="mx-2 hover:underline">Home</Link>
+          <Link to="/schemes" className="mx-2 hover:underline">Schemes</Link>
+          <Link to="/grievance" className="mx-2 hover:underline">Grievance</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
